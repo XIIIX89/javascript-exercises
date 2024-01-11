d7ef2bf1aa020845defd521c55e009a4fad0accc
XIIIX89 <114373945+XIIIX89@users.noreply.github.com>
2024-01-11 13:57:36 -0800

    completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // first two fibonacci numbers are 0 and 1
+    let a = 0;
+    let b = 1;
+    
+    if (typeof num !== Number) {num = parseInt(num);} // turn strings into numbers
+    if (num < 0) {return 'OOPS'} // does not accept negative numbers
 
+    for (let i = 2; i <= num; i++) {
+        let nextNum = a + b;
+        a = b;
+        b = nextNum;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = books.reduce((title, item) => {
+        title.push(item.title);
+        return title;
+    }, []); // initializes title as a blank array
+    return titles;
 };
 
 // Do not edit below this line
